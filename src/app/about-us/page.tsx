7d4ee37de0f2762d0b01c546b56d4d4c62e57294
Ishowar84 <ishowardev84@gmail.com>
2026-03-02 12:46:13 +0545

    small change to trigger auto deploy

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -252,7 +252,7 @@ export default function AboutUsPage() {
                                 <div className="space-y-6">
                                     {[
                                         { label: "Our Office", val: "Denver, Colorado", icon: <Building2 className="w-5 h-5 text-brand-accent" /> },
-                                        //{ label: "Global Offices", val: "Gatthaghar, Nepal", icon: <Search className="w-5 h-5 text-brand-primary" /> },
+                                        //{ label: "Global Offices", val: "Gatthaghar, Nepal", icon: <Search className="w-5 h-5 text-brand-accent" /> },
                                         { label: "Contact Us", val: "hello@press1.dev", icon: <Mail className="w-5 h-5 text-brand-accent" /> },
                                     ].map((item, i) => (
                                         <div key={i} className="flex items-center gap-4">
